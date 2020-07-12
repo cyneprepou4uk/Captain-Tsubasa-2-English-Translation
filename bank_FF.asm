9d46af175e6f48c58987f5bf195be267fcd9b770
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-07-13 01:17:14 +0300

    Revert "удалены 0009 и 000A"
    This reverts commit e303d9894ad8f20474e19a86537d8a6a36aaca02.

--- bank_FF.asm
+++ bank_FF.asm
@@ -455,6 +455,9 @@ C - - - - 0x03C6D0 FF:C6C0: 9A        TXS
 C - - - - 0x03C6D1 FF:C6C1: A9 00     LDA #$00
 C - - - - 0x03C6D3 FF:C6C3: 85 01     STA ram_0001
 C - - - - 0x03C6D5 FF:C6C5: 85 02     STA ram_0002
+C - - - - 0x03C6DD FF:C6CD: A9 00     LDA #$00
+C - - - - 0x03C6DF FF:C6CF: 85 09     STA ram_0009
+C - - - - 0x03C6E1 FF:C6D1: 85 0A     STA ram_000A
 C - - - - 0x03C6E3 FF:C6D3: A9 00     LDA #$00
 C - - - - 0x03C6E5 FF:C6D5: 85 0D     STA ram_000D
 C - - - - 0x03C6E7 FF:C6D7: 85 0E     STA ram_000E
