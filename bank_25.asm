e85ab77ff88fed844ca3b4f4a7de0b983ce56a6f
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-07-17 19:25:03 +0300

    перевод первого vs экрана

--- bank_25.asm
+++ bank_25.asm
@@ -44,78 +44,99 @@ off_матч_01:
 	.byte $0D
 ; верхняя горизонтальная линия
 	.byte $05
-	.word $2145
+	.word $2185
 	.byte $16
 	.byte $8E
 ; центральная горизонтальная линия
 	.byte $05
-	.word $21A5
+	.word $21E5
 	.byte $16
 	.byte $BA
 ; нижняя горизонтальная линия
 	.byte $05
-	.word $22A5
+	.word $22E5
 	.byte $16
 	.byte $8E
 ; левая вертикальная линия
 	.byte $04
-	.word $2164
+	.word $21A4
 	.byte $0A
 	.byte $8F
 ; центральная вертикальная линия
 	.byte $04
-	.word $21CF
+	.word $220F
 	.byte $07
 	.byte $BB
 ; правая вертикальная линия
 	.byte $04
-	.word $217B
+	.word $21BB
 	.byte $0A
 	.byte $8F
 	
 	.byte $00
 	.word @углы
 	
-	;.byte $00
-	;.word @текст
+	.byte $00
+	.word @текст
 	
 	.byte $0F
+
+
 
 @углы:
 ; левый верхний угол
 	.byte $01
-	.word $2144
+	.word $2184
 	.byte $90
 ; правый верхний угол
 	.byte $01
-	.word $215B
+	.word $219B
 	.byte $91
 ; правый нижний угол
 	.byte $01
-	.word $22A4
+	.word $22E4
 	.byte $92
 ; левый нижний угол
 	.byte $41
-	.word $22BB
+	.word $22FB
 	.byte $93
 
+
+
 @текст:
-	.byte $01
-	.word $2144
-	.byte $88
-	.byte $01
-	.word $215B
-	.byte $90
-	.byte $01
-	.word $22A4
-	.byte $8E
-	.byte $01
-	.word $22BB
-	.byte $93
-	.byte $42
-	.word $2233
-	.byte $91
-	.byte $8F
+	.byte $09
+	.word $20EC
+	.text "Episode 1"
+	
+	.byte $1C
+	.word $2122
+	.byte $7B
+	.text "#10,Sao Paulo's Lucky Star"
+	.byte $7B
+	
+	.byte $0F
+	.word $21C9
+	.text "Rio Cup, Brazil"
+	
+	.byte $04
+	.word $2247
+	.text "1/32"
+	
+	.byte $05
+	.word $2287
+	.text "final"
+	
+	.byte $09
+	.word $2230
+	.text "Sao Paulo"
+	
+	.byte $02
+	.word $2274
+	.text "VS"
+	
+	.byte $4A
+	.word $22B0
+	.text "Fluminence"
 
 
 
