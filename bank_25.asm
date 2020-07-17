f0f4cc2aa2a03794eb199f5132f02ab6278c035a
cyneprepou4uk <cyneprepou4uk@gmail.com>
2020-07-17 20:23:39 +0300

    перевод

--- bank_25.asm
+++ bank_25.asm
@@ -2,7 +2,6 @@
 .include "copy_bank_ram.inc"
 ; таблицы VS экрана
 
-
 .export tbl_0x040003_vs_экран
 tbl_0x040003_vs_экран:
 	.word off_матч_01
@@ -39,6 +38,11 @@ tbl_0x040003_vs_экран:
 	.word off_матч_32
 	.word off_матч_33
 
+
+
+
+
+
 off_матч_01:
 ; очистить nametable
 	.byte $0D
@@ -64,7 +68,7 @@ off_матч_01:
 	.byte $8F
 ; центральная вертикальная линия
 	.byte $04
-	.word $220F
+	.word $220E
 	.byte $07
 	.byte $BB
 ; правая вертикальная линия
@@ -78,7 +82,7 @@ off_матч_01:
 	
 	.byte $00
 	.word @текст
-	
+; закончить
 	.byte $0F
 
 
@@ -118,9 +122,9 @@ off_матч_01:
 	.word $21C9
 	.text "Rio Cup, Brazil"
 	
-	.byte $04
+	.byte $05
 	.word $2247
-	.text "1/32"
+	.text "1/32-"
 	
 	.byte $05
 	.word $2287
@@ -131,21 +135,534 @@ off_матч_01:
 	.text "Sao Paulo"
 	
 	.byte $02
-	.word $2274
+	.word $2273
 	.text "VS"
 	
 	.byte $4A
-	.word $22B0
+	.word $22AF
 	.text "Fluminence"
 
 
 
 
+
+
 off_матч_02:
+; очистить nametable
+	.byte $0D
+; верхняя горизонтальная линия
+	.byte $05
+	.word $2185
+	.byte $16
+	.byte $8E
+; центральная горизонтальная линия
+	.byte $05
+	.word $21E5
+	.byte $16
+	.byte $BA
+; нижняя горизонтальная линия
+	.byte $05
+	.word $22E5
+	.byte $16
+	.byte $8E
+; левая вертикальная линия
+	.byte $04
+	.word $21A4
+	.byte $0A
+	.byte $8F
+; центральная вертикальная линия
+	.byte $04
+	.word $220E
+	.byte $07
+	.byte $BB
+; правая вертикальная линия
+	.byte $04
+	.word $21BB
+	.byte $0A
+	.byte $8F
+	
+	.byte $00
+	.word @углы
+	
+	.byte $00
+	.word @текст
+; закончить
+	.byte $0F
+
+
+
+@углы:
+; левый верхний угол
+	.byte $01
+	.word $2184
+	.byte $90
+; правый верхний угол
+	.byte $01
+	.word $219B
+	.byte $91
+; правый нижний угол
+	.byte $01
+	.word $22E4
+	.byte $92
+; левый нижний угол
+	.byte $41
+	.word $22FB
+	.byte $93
+
+
+
+@текст:
+	.byte $09
+	.word $20EC
+	.text "Episode 2"
+	
+	.byte $1C
+	.word $2122
+	.byte $7B
+	.text "                          "
+	.byte $7B
+	
+	.byte $0F
+	.word $21C9
+	.text "Rio Cup, Brazil"
+	
+	.byte $05
+	.word $2247
+	.text "1/16-"
+	
+	.byte $05
+	.word $2287
+	.text "final"
+	
+	.byte $09
+	.word $2230
+	.text "Sao Paulo"
+	
+	.byte $02
+	.word $2273
+	.text "VS"
+	
+	.byte $4B
+	.word $22AF
+	.text "Corinthians"
+
+
+
+
+
+
 off_матч_03:
+; очистить nametable
+	.byte $0D
+; верхняя горизонтальная линия
+	.byte $05
+	.word $2185
+	.byte $16
+	.byte $8E
+; центральная горизонтальная линия
+	.byte $05
+	.word $21E5
+	.byte $16
+	.byte $BA
+; нижняя горизонтальная линия
+	.byte $05
+	.word $22E5
+	.byte $16
+	.byte $8E
+; левая вертикальная линия
+	.byte $04
+	.word $21A4
+	.byte $0A
+	.byte $8F
+; центральная вертикальная линия
+	.byte $04
+	.word $220E
+	.byte $07
+	.byte $BB
+; правая вертикальная линия
+	.byte $04
+	.word $21BB
+	.byte $0A
+	.byte $8F
+	
+	.byte $00
+	.word @углы
+	
+	.byte $00
+	.word @текст
+; закончить
+	.byte $0F
+
+
+
+@углы:
+; левый верхний угол
+	.byte $01
+	.word $2184
+	.byte $90
+; правый верхний угол
+	.byte $01
+	.word $219B
+	.byte $91
+; правый нижний угол
+	.byte $01
+	.word $22E4
+	.byte $92
+; левый нижний угол
+	.byte $41
+	.word $22FB
+	.byte $93
+
+
+
+@текст:
+	.byte $09
+	.word $20EC
+	.text "Episode 3"
+	
+	.byte $1C
+	.word $2122
+	.byte $7B
+	.text "                          "
+	.byte $7B
+	
+	.byte $0F
+	.word $21C9
+	.text "Rio Cup, Brazil"
+	
+	.byte $04
+	.word $2247
+	.text "1/8-"
+	
+	.byte $05
+	.word $2287
+	.text "final"
+	
+	.byte $09
+	.word $2230
+	.text "Sao Paulo"
+	
+	.byte $02
+	.word $2273
+	.text "VS"
+	
+	.byte $46
+	.word $22B1
+	.text "Gremio"
+
+
+
+
+
+
 off_матч_04:
+; очистить nametable
+	.byte $0D
+; верхняя горизонтальная линия
+	.byte $05
+	.word $2185
+	.byte $16
+	.byte $8E
+; центральная горизонтальная линия
+	.byte $05
+	.word $21E5
+	.byte $16
+	.byte $BA
+; нижняя горизонтальная линия
+	.byte $05
+	.word $22E5
+	.byte $16
+	.byte $8E
+; левая вертикальная линия
+	.byte $04
+	.word $21A4
+	.byte $0A
+	.byte $8F
+; центральная вертикальная линия
+	.byte $04
+	.word $220E
+	.byte $07
+	.byte $BB
+; правая вертикальная линия
+	.byte $04
+	.word $21BB
+	.byte $0A
+	.byte $8F
+	
+	.byte $00
+	.word @углы
+	
+	.byte $00
+	.word @текст
+; закончить
+	.byte $0F
+
+
+
+@углы:
+; левый верхний угол
+	.byte $01
+	.word $2184
+	.byte $90
+; правый верхний угол
+	.byte $01
+	.word $219B
+	.byte $91
+; правый нижний угол
+	.byte $01
+	.word $22E4
+	.byte $92
+; левый нижний угол
+	.byte $41
+	.word $22FB
+	.byte $93
+
+
+
+@текст:
+	.byte $09
+	.word $20EC
+	.text "Episode 4"
+	
+	.byte $1C
+	.word $2122
+	.byte $7B
+	.text "                          "
+	.byte $7B
+	
+	.byte $0F
+	.word $21C9
+	.text "Rio Cup, Brazil"
+	
+	.byte $07
+	.word $2246
+	.text "Quater-"
+	
+	.byte $05
+	.word $2287
+	.text "final"
+	
+	.byte $09
+	.word $2230
+	.text "Sao Paulo"
+	
+	.byte $02
+	.word $2273
+	.text "VS"
+	
+	.byte $49
+	.word $22B0
+	.text "Palmeiras"
+
+
+
+
+
+
 off_матч_05:
+; очистить nametable
+	.byte $0D
+; верхняя горизонтальная линия
+	.byte $05
+	.word $2185
+	.byte $16
+	.byte $8E
+; центральная горизонтальная линия
+	.byte $05
+	.word $21E5
+	.byte $16
+	.byte $BA
+; нижняя горизонтальная линия
+	.byte $05
+	.word $22E5
+	.byte $16
+	.byte $8E
+; левая вертикальная линия
+	.byte $04
+	.word $21A4
+	.byte $0A
+	.byte $8F
+; центральная вертикальная линия
+	.byte $04
+	.word $220E
+	.byte $07
+	.byte $BB
+; правая вертикальная линия
+	.byte $04
+	.word $21BB
+	.byte $0A
+	.byte $8F
+	
+	.byte $00
+	.word @углы
+	
+	.byte $00
+	.word @текст
+; закончить
+	.byte $0F
+
+
+
+@углы:
+; левый верхний угол
+	.byte $01
+	.word $2184
+	.byte $90
+; правый верхний угол
+	.byte $01
+	.word $219B
+	.byte $91
+; правый нижний угол
+	.byte $01
+	.word $22E4
+	.byte $92
+; левый нижний угол
+	.byte $41
+	.word $22FB
+	.byte $93
+
+
+
+@текст:
+	.byte $09
+	.word $20EC
+	.text "Episode 5"
+	
+	.byte $1C
+	.word $2122
+	.byte $7B
+	.text "                          "
+	.byte $7B
+	
+	.byte $0F
+	.word $21C9
+	.text "Rio Cup, Brazil"
+	
+	.byte $05
+	.word $2247
+	.text "Semi-"
+	
+	.byte $05
+	.word $2287
+	.text "final"
+	
+	.byte $09
+	.word $2230
+	.text "Sao Paulo"
+	
+	.byte $02
+	.word $2273
+	.text "VS"
+	
+	.byte $46
+	.word $22B1
+	.text "Santos"
+
+
+
+
+
+
 off_матч_06:
+; очистить nametable
+	.byte $0D
+; верхняя горизонтальная линия
+	.byte $05
+	.word $2185
+	.byte $16
+	.byte $8E
+; центральная горизонтальная линия
+	.byte $05
+	.word $21E5
+	.byte $16
+	.byte $BA
+; нижняя горизонтальная линия
+	.byte $05
+	.word $22E5
+	.byte $16
+	.byte $8E
+; левая вертикальная линия
+	.byte $04
+	.word $21A4
+	.byte $0A
+	.byte $8F
+; центральная вертикальная линия
+	.byte $04
+	.word $220E
+	.byte $07
+	.byte $BB
+; правая вертикальная линия
+	.byte $04
+	.word $21BB
+	.byte $0A
+	.byte $8F
+	
+	.byte $00
+	.word @углы
+	
+	.byte $00
+	.word @текст
+; закончить
+	.byte $0F
+
+
+
+@углы:
+; левый верхний угол
+	.byte $01
+	.word $2184
+	.byte $90
+; правый верхний угол
+	.byte $01
+	.word $219B
+	.byte $91
+; правый нижний угол
+	.byte $01
+	.word $22E4
+	.byte $92
+; левый нижний угол
+	.byte $41
+	.word $22FB
+	.byte $93
+
+
+
+@текст:
+	.byte $09
+	.word $20EC
+	.text "Episode 6"
+	
+	.byte $19
+	.word $2124
+	.byte $7B
+	.text "Son of the Football God"
+	.byte $7B
+	
+	.byte $0F
+	.word $21C9
+	.text "Rio Cup, Brazil"
+	
+	.byte $05
+	.word $2267
+	.text "Final"
+	
+	.byte $09
+	.word $2230
+	.text "Sao Paulo"
+	
+	.byte $02
+	.word $2273
+	.text "VS"
+	
+	.byte $48
+	.word $22B0
+	.text "Flamengo"
+
+
+
+
+
+
 off_матч_07:
 off_матч_08:
 off_матч_09:
